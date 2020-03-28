01c9a0c5407b17a601279b7f892898f01c198de3
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-03-28 13:15:59 +0100

    updated

--- assets/README.md
+++
@@ -1,1 +0,0 @@
-Illustrationer til opgave beskrivelsen!
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'themegray': '#F0F3F4',
+        'theme': '#D97D54',
+        'themeblue': '#6E8CA0'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
